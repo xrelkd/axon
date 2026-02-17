d8c07521df767033f71083446aec737aec6bbea9
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-02-17 19:22:42 +0800

    feat: initial commit

---
+++ devshell/format.nix
@@ -0,0 +1,33 @@
+{ pkgs }:
+
+pkgs.runCommand "check-format"
+  {
+    buildInputs = with pkgs; [
+      fd
+
+      shellcheck
+
+      buf
+      nixfmt
+      nodePackages.prettier
+      shfmt
+      taplo
+      treefmt
+    ];
+  }
+  ''
+    treefmt \
+      --allow-missing-formatter \
+      --fail-on-change \
+      --no-cache \
+      --formatters prettier \
+      --formatters protobuf \
+      --formatters nix \
+      --formatters shell \
+      --formatters hcl \
+      --formatters toml \
+      -C ${./..}
+
+    # it worked!
+    touch $out
+  ''
