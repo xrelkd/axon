06c8c872ca49ee9fa4a73ee4ce05c2ff756c61c3
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-02-19 12:05:06 +0800

    wip

--- axon/src/config/mod.rs
+++ axon/src/config/mod.rs
@@ -1,19 +1,18 @@
 mod image_pull_policy;
 mod port_mapping;
 mod service_ports;
+mod spec;
 
-use std::{
-    net::{IpAddr, Ipv4Addr},
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
-use axon_base::{PROJECT_NAME, consts};
+use axon_base::PROJECT_NAME;
 use resolve_path::PathResolveExt;
 use serde::{Deserialize, Serialize};
 use snafu::{ResultExt, Snafu};
 
 pub use self::{
     image_pull_policy::ImagePullPolicy, port_mapping::PortMapping, service_ports::ServicePorts,
+    spec::Spec,
 };
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -106,74 +105,6 @@ impl Config {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Spec {
-    pub name: String,
-
-    #[allow(clippy::struct_field_names)]
-    pub image: String,
-
-    #[allow(clippy::struct_field_names)]
-    #[serde(default)]
-    pub image_pull_policy: ImagePullPolicy,
-
-    #[serde(default)]
-    pub port_mappings: Vec<PortMapping>,
-
-    #[serde(default)]
-    pub service_ports: ServicePorts,
-
-    #[serde(default)]
-    pub command: Vec<String>,
-
-    #[serde(default)]
-    pub args: Vec<String>,
-
-    #[serde(default)]
-    pub interactive_shell: Vec<String>,
-}
-
-impl Spec {
-    fn spec_malm() -> Self {
-        Self {
-            name: "malm".to_string(),
-            image: "ghcr.io/xrelkd/malm:0.7.0".to_string(),
-            image_pull_policy: ImagePullPolicy::IfNotPresent,
-            port_mappings: vec![
-                PortMapping {
-                    container_port: 8080,
-                    local_port: 8080,
-                    address: IpAddr::V4(Ipv4Addr::LOCALHOST),
-                },
-                PortMapping {
-                    container_port: 22,
-                    local_port: 22222,
-                    address: IpAddr::V4(Ipv4Addr::LOCALHOST),
-                },
-            ],
-            service_ports: ServicePorts { ssh: Some(22), http: Some(8080), https: None },
-            command: Vec::new(),
-            args: Vec::new(),
-            interactive_shell: vec!["/bin/zsh".to_string()],
-        }
-    }
-}
-
-impl Default for Spec {
-    fn default() -> Self {
-        Self {
-            name: PROJECT_NAME.to_string(),
-            image: consts::DEFAULT_IMAGE.to_string(),
-            image_pull_policy: ImagePullPolicy::default(),
-            port_mappings: Vec::new(),
-            service_ports: ServicePorts::default(),
-            command: vec!["sh".to_string()],
-            args: vec!["-c".to_string(), "while true; do sleep 1; done".to_string()],
-            interactive_shell: vec!["/bin/sh".to_string()],
-        }
-    }
-}
 fn default_pod_name() -> String { PROJECT_NAME.to_string() }
 
 fn default_spec() -> String { PROJECT_NAME.to_string() }
